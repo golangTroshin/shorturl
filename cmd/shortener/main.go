bfa834d59ce98fcdd55e0e3f5c320d1b2c133dc2
Vladimir Troshin <troshinvladimira@gmail.com>
2024-06-27 15:11:35 +0200

    1st iteration

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,78 @@
 package main
 
-func main() {}
+import (
+	"encoding/base64"
+	"io"
+	"net/http"
+	"strings"
+)
+
+const ContentTypePlainText = "text/plain"
+
+var urlMap = make(map[string]string)
+
+func main() {
+	if err := run(); err != nil {
+		panic(err)
+	}
+}
+
+func run() error {
+	mux := http.NewServeMux()
+	mux.Handle(`/{id}`, contentTypeMiddleware(http.HandlerFunc(getHandler)))
+	mux.Handle(`/`, contentTypeMiddleware(http.HandlerFunc(postHandler)))
+
+	return http.ListenAndServe(`:8080`, mux)
+}
+
+func contentTypeMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// if r.Header.Get("Content-Type") != ContentTypePlainText {
+		// 	http.Error(w, "Unsupported Media Type", http.StatusBadRequest)
+		// 	return
+		// }
+
+		w.Header().Set("Content-Type", "text/plain")
+		next.ServeHTTP(w, r)
+	})
+}
+
+func getHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		return
+	}
+
+	parts := strings.Split(r.URL.Path, "/")
+
+	if len(parts) != 2 {
+		http.Error(w, "Invalid URL", http.StatusBadRequest)
+		return
+	}
+
+	key := parts[1]
+
+	if val, ok := urlMap[key]; ok {
+		w.Header().Set("Location", val)
+	}
+
+	w.WriteHeader(http.StatusTemporaryRedirect)
+}
+
+func postHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		return
+	}
+
+	body, err := io.ReadAll(r.Body)
+
+	if err != nil || len(body) == 0 {
+		http.Error(w, "Empty body", http.StatusBadRequest)
+		return
+	}
+
+	key := base64.StdEncoding.EncodeToString(body)[:8]
+	urlMap[key] = string(body)
+
+	w.WriteHeader(http.StatusCreated)
+	w.Write([]byte("http://" + r.Host + "/" + key))
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/golangTroshin/shorturl
+
+go 1.22.4
